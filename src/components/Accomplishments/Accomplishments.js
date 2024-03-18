893d8e366831b9cc5ce7a92e948673bd88ad0c7c
AravindSomaraj <aravindsomaraj101@gmail.com>
2024-03-18 15:40:26 +0530

    add components

---
+++ src/components/Accomplishments/Accomplishments.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+
+const data = [
+  { number: 20, text: 'Jusst containers of data'},
+];
+
+const Acomplishments = () => (
+  <div>
+    Acomplishments
+  </div>
+);
+
+export default Acomplishments;
