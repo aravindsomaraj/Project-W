37e9b3f32c9639adfb49332af6315bec0a621059
AravindSomaraj <aravindsomaraj101@gmail.com>
2024-03-18 15:23:29 +0530

    add resources

---
+++ src/constants/constants.js
@@ -0,0 +1,15 @@
+export const projects = [
+    {
+      title: 'Random Thign #1',
+      description: "Totally short description yes",
+        image: 'image.png',
+        tags: ['Tags for things'],
+      source: 'https://google.com',
+      visit: 'https://google.com',
+      id: 0,
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2024, text: 'doing this?', },
+  ];
